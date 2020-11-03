9685468f753845f9a7bc78afba68925ba0000898
Veronika Dodda <v.shengeliya@gmail.com>
2020-11-03 14:42:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-// Code your solution here!
+// The substring() method returns the part of the string 
+//between the start and end indexes, or to the end of the string.
 
 function printString (myString) {
     console.log(myString[0])
     if (myString.length > 1){
         let mySubString = myString.substring(1, myString.length)
-        console.log("my substring", mySubString)
         printString(mySubString)
     } else {
         return true
+    }
+}
+
+function reverseString(myString){
+    if (myString === ""){
+       return "";
+    } else {
+        return reverseString(myString.substring(1))+ myString.charAt(0)
     }
 }
 
