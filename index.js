a2dba658f709614dd79ea1a7dc606535792e1fd0
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-11-15 15:44:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // Code your solution here!
 
+// prompt" Write a recursive function to print out all of the characters in a string.
+
+
+//writing it to work for a single example
+// function printString(myString) {
+//     console.log(myString[0], myString[1], myString[2], myString[3], myString[4]);
+// }
+
+// printString("pizza")
+
+//abstracting it using recursion
+
+function printString(myString) {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  }
+
+  
